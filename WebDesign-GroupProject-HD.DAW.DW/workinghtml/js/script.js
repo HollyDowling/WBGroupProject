55ff9f00f7443156423598ad669a660cbf07731b
dlndavis020 <dlndavis020@gmail.com>
2022-08-10 13:41:23 +0100

    Add files via upload
    Working js

---
+++ WebDesign-GroupProject-HD.DAW.DW/workinghtml/js/script.js
@@ -0,0 +1,26 @@
+let toss = document.getElementById('theflip');
+function flip(){
+    if(Math.random() < 0.5){
+        toss.innerHTML = "You got Heads";
+
+    }
+    else{
+        toss.innerHTML = "You got Tails";
+
+    }
+}
+document.getElementById('clicker').onclick = flip;
+
+// Was trying to make a counter to tick up everytime you got heads but couldnt make it work.
+/* let score = document.getElementById('thescore')
+var func1 = flip();
+function click(){
+    if(func1 == "You got Heads"){
+        score++;
+        score.innerHTML;
+    }
+    else{
+        score--;
+        score.innerHTML;
+    }
+} */
